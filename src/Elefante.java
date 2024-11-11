9cade4573877908033003fe332837c951c48c772
caique mendes <caiquemendes013@gmail.com>
2024-11-10 23:54:17 -0300

    Esqueleto inicial

---
+++ src/Elefante.java
@@ -0,0 +1,16 @@
+public class Elefante extends Animal implements ComportamentoAlimentar{
+
+    public Elefante(String nome, int idade) {
+        super(nome, idade);
+    }
+
+    @Override
+    void fazerSom() {
+
+    }
+
+    @Override
+    public void alimentar() {
+
+    }
+}
---
+++ src/Leao.java
@@ -0,0 +1,16 @@
+public class Leao extends Animal implements ComportamentoAlimentar{
+
+    public Leao(String nome, int idade) {
+        super(nome, idade);
+    }
+
+    @Override
+    void fazerSom() {
+
+    }
+
+    @Override
+    public void alimentar() {
+
+    }
+}
